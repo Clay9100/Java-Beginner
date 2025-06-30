3119f9aa50d20fea1dd92ade386edb2bae01c77f
Muhammadsobir <muhammadsobirturdaliyev9199@gmail.com>
2025-06-30 10:41:13 -0700

    added String Variable

--- String/src/Main.java
+++ String/src/Main.java
@@ -5,13 +5,22 @@ public class Main {
 
        // String data type
 
-        String str = "Hello World";
+        /*String str = "Hello World";
         String str2 = "Hello do you speak English";
         String str3 = "Yes I'm";
 
         System.out.println(str);
         System.out.println(str2);
-        System.out.println(str3);
+        System.out.println(str3);*/
+
+        String name = "Muhammadsobir";
+        String wordNumber = "12341";
+        String phoneNumber = "+9989545337788";
+
+        System.out.println(name);
+        System.out.println(wordNumber);
+        System.out.println(phoneNumber);
+
 
 
 
