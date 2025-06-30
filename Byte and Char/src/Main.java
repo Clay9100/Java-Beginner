3faa391ddb604751fd3565827965b07ec8d5baf2
Muhammadsobir <muhammadsobirturdaliyev9199@gmail.com>
2025-06-30 06:48:02 -0700

    added data type byte error value

--- Byte and Char/src/Main.java
+++ Byte and Char/src/Main.java
@@ -8,12 +8,20 @@ public class Main {
 
         // byte from -128 to 127
 
-        byte age = 35;
+        /*byte age = 35;
         byte month = 7;
         byte day = 1;
         System.out.println("Age: " + age);
         System.out.println("Month: " + month);
-        System.out.println("Day: " + day);
+        System.out.println("Day: " + day);*/
+
+        // error
+       // byte age = 130;
+        // byte km = 300;
+        // byte m = -400;
+
+
+
 
 
     }
