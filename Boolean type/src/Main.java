82f826354a8dbd748c037773e3f138c137a7fbcf
Muhammadsobir <muhammadsobirturdaliyev9199@gmail.com>
2025-06-30 05:57:34 -0700

    added new lesson date type is Boolean

---
+++ Boolean type/src/Main.java
@@ -0,0 +1,22 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        // This is lesson boolean date type
+        boolean isOpen = true;
+        boolean isClosed = false;
+
+        System.out.println("Door is open: " + isOpen);
+        System.out.println("Door is closed: " + isClosed);
+
+        boolean sunny = true;
+        System.out.println("Sunny is: " + sunny);
+
+        boolean night = false;
+        System.out.println("Night is: " + night);
+
+
+
+    }
+}
