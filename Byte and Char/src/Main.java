27b82b412cf32201d5088b5206e5cefeee398e49
Muhammadsobir <muhammadsobirturdaliyev9199@gmail.com>
2025-06-30 06:42:19 -0700

    added new data type byte Variables age, month, day

--- Byte and Char/src/Main.java
+++ Byte and Char/src/Main.java
@@ -4,6 +4,17 @@ public class Main {
 
         // This is lesson byte and char
 
+        // byte data type
+
+        // byte from -128 to 127
+
+        byte age = 35;
+        byte month = 7;
+        byte day = 1;
+        System.out.println("Age: " + age);
+        System.out.println("Month: " + month);
+        System.out.println("Day: " + day);
+
 
     }
 }
