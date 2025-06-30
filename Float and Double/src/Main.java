7e01664cc772fd50c21fdd86ec3c215adcbab85d
Muhammadsobir <muhammadsobirturdaliyev9199@gmail.com>
2025-06-30 10:26:46 -0700

    added double data type variable

--- Float and Double/src/Main.java
+++ Float and Double/src/Main.java
@@ -7,12 +7,23 @@ public class Main {
 
         // float
 
-        float pi = 3.14f;
+        /*float pi = 3.14f;
         float e = 2.71f;
         float g = 9.81f;
         System.out.println("pi: "+ pi);
         System.out.println("e: "+ e);
-        System.out.println("g: "+ g);
+        System.out.println("g: "+ g);*/
+
+        // double
+
+        double d1 = 34.56;
+        double d2 = 3.68799;
+        double d3 = -3489.5647648;
+
+        System.out.println("d1: " + d1);
+        System.out.println("d2: " + d2);
+        System.out.println("d3: " + d3);
+
 
 
 
