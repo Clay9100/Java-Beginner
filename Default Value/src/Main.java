e18877b96a8bacba0e6c595522543b8c1c8146a4
Muhammadsobir <muhammadsobirturdaliyev9199@gmail.com>
2025-07-01 10:16:57 -0700

    added default value

--- Default Value/src/Main.java
+++ Default Value/src/Main.java
@@ -1,9 +1,22 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    static int number;
+    static String name;
+    static float salary;
+    static double weight;
     public static void main(String[] args) {
 
         // This lesson default value
+        int age;
+        //System.out.println(age); error because not value age
+
+        System.out.println(number); // int -> 0
+        System.out.println(name); // String -> null
+        System.out.println(salary); // float -> 0.0
+        System.out.println(weight); // double -> 0.0
+
 
 
 
