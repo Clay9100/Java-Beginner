d400e4620424e080f3aba1ec1eb9a31225852f6e
clay <muhammadsobirturdaliyev9199@gmail.com>
2025-07-01 08:54:23 +0500

    create global variable in Main class

--- Global and Local/src/Main.java
+++ Global and Local/src/Main.java
@@ -1,6 +1,12 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    // global variable
+    static int number = 23;
+    static String word = "This is variable global";
+    static char lesser = '%';
+
     public static void main(String[] args) {
 
         // This is lesson Global and Local
@@ -8,13 +14,17 @@ public class Main {
 
         // Local Variable
 
-        int age = 9;
+        /*int age = 9;
         String name = "Axror";
         long l = 12343234;
 
         System.out.println(age);
         System.out.println(name);
-        System.out.println(l);
+        System.out.println(l);*/
+
+        System.out.println(number);
+        System.out.println(lesser);
+        System.out.println(word);
 
 
     }
