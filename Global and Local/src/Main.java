70dff0c97a977769bdc5b5690617c0051da71af9
clay <muhammadsobirturdaliyev9199@gmail.com>
2025-07-01 08:39:41 +0500

    change print

--- Global and Local/src/Main.java
+++ Global and Local/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         // This is lesson Global and Local
-        System.out.println("Hello World!");
+        System.out.println("This is lesson Global and Local");
 
     }
 }
