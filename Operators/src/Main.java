b6f3aea9801fdea189ba597955085125545d1b82
Muhammadsobir <muhammadsobirturdaliyev9199@gmail.com>
2025-07-01 11:31:34 -0700

    added Operators + - / * %

--- Operators/src/Main.java
+++ Operators/src/Main.java
@@ -5,6 +5,76 @@ public class Main {
 
        // this is lesson operators
 
+       // Arithmetic + - / * %
+
+        // +
+
+        /*System.out.println(2 + 3);
+
+        int a = 1;
+        int b = 4;
+        System.out.println(a + b);
+
+        int s = 10;
+        int s1 = s + 10;
+        System.out.println(s1);*/
+
+        // -
+
+        /*int a = 7;
+        int b = 8;
+        System.out.println(a - b);
+        System.out.println(56 - 8);*/
+
+        // /
+
+        /*int a = 9;
+        int b = 3;
+        System.out.println(a / b);
+        System.out.println((double)(34 / 3));
+
+        double k = 67 / 5.0;
+        System.out.println(k);*/
+
+        // *
+
+       /* int a = 9;
+        int b = 2;
+        System.out.println(a * b);
+        System.out.println(3 * 4);
+
+        int x = 3;
+        int y = x * 5;
+        System.out.println(y);*/
+
+        // %
+
+        System.out.println(5 % 7);
+        int a = 9;
+        int b = 2;
+        System.out.println(a % b);
+
+        int x = 23;
+        int y = 5;
+
+        int mod = x % y;
+        System.out.println(mod);
+
+
+
+
+       // Assignments =,
+
+       // Comparison < > <= >= !=
+
+       // Logic && | !
+
+       // Increment k++
+
+       // Decrement --k
+
+
+
 
     }
 }
