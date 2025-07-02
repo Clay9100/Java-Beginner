0f040d932021a3bcecbdd279da834d0b94e8da3f
Muhammadsobir <muhammadsobirturdaliyev9199@gmail.com>
2025-07-02 09:59:54 -0700

    added if else if else

--- If else if/src/Main.java
+++ If else if/src/Main.java
@@ -2,9 +2,34 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+
         // this is lesson if else
+
+        /*int x = 7;
+
+        if(x > 0){
+            System.out.println("x is greater than 0");
+        }else {
+            System.out.println("x is less than 0");
+        }*/
+
+        /*int a = -9, b = 5;
+
+        if(a > b){
+            System.out.println("a big number");
+        }else {
+            System.out.println("b big number");
+        }*/
+
+        /*int x = 0;
+
+        if(x > 0){
+            System.out.println("x is greater than 0");
+        } else if (x < 0) {
+            System.out.println("x is less than 0");
+        }else {
+            System.out.println("x is 0");
+        }*/
 
 
     }
