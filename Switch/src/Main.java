4cae3a33ae12c2318b6ee12e424be9b6298f8343
Muhammadsobir <muhammadsobirturdaliyev9199@gmail.com>
2025-07-02 10:27:55 -0700

    added month Winter, Spring, Summer, Autumn

--- Switch/src/Main.java
+++ Switch/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
        // this is lesson switch operator
 
-        int day = 1;
+      /*  int day = 1;
         switch (day) {
             case 1:
                 System.out.println("Monday");
@@ -28,6 +28,35 @@ public class Main {
             case 7:
                 System.out.println("Sunday");
                 break;
+        }*/
+
+
+        int month = 7;
+
+        switch (month) {
+            case 1:
+            case 2:
+            case 12:
+                System.out.println("Winter");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Spring");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Summer");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Autumn");
+                break;
+            default:
+                System.out.println("not found ");
+
         }
 
 
