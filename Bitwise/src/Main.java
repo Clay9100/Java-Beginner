5d527bab0052bb89c7bfc15b7875d3c5325df67e
Muhammadsobir <muhammadsobirturdaliyev9199@gmail.com>
2025-07-02 06:26:22 -0700

    added and(&)

--- Bitwise/src/Main.java
+++ Bitwise/src/Main.java
@@ -5,5 +5,18 @@ public class Main {
 
        // This is lesson Bitwise operators
 
+        // Bitwise and(&) or(|) xor(^) left(>>) right(<<)
+
+        // and(&)
+
+        /*System.out.println(5 & 5); // 5
+        int x = 9, y = 7; // 1001 && 0111 => 0001
+        System.out.println(x & y); // 1
+
+        int a = 8, b = 10; // a = 1000 & 1010 => 1000
+        System.out.println(a & b); // 8*/
+
+
+
     }
 }
