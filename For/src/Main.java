aa9655ef64f628629bc597333aa87cb661574377
Muhammadsobir <muhammadsobirturdaliyev9199@gmail.com>
2025-07-04 02:45:51 -0700

    added int char String

--- For/src/Main.java
+++ For/src/Main.java
@@ -6,6 +6,27 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         // this is lesson for operator
 
+        // for loop
+
+        /*for (int i = 1; i < 10; i++){
+            System.out.println(i);
+        }*/
+
+        // for char
+
+        /*for (char i = 'A'; i <= 'Z' ; i++) {
+            System.out.println(i);
+        }*/
+
+        // for String
+
+        /*String name = "Muhammadsobir";
+        for (int i = 0; i < name.length(); i++) {
+            System.out.println(name.charAt(i));
+        }*/
+
+        // float
+
 
     }
 }
