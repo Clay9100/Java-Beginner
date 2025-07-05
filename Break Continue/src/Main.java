a160309279e2256d50931a516c096928f103298e
Muhammadsobir <muhammadsobirturdaliyev9199@gmail.com>
2025-07-05 11:09:46 -0700

    added new lesson break continue

---
+++ Break Continue/src/Main.java
@@ -0,0 +1,30 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+       // this is lesson break continue
+
+        /*for (int i = 0; i < 10; i++) {
+
+            if (i == 5) {
+                break;
+            }
+            System.out.println(i);
+        }*/
+
+       /* int n = 0;
+
+        while (n < 100){
+            System.out.println("n: " + n);
+
+            if (n == 27){
+                break;
+            }
+            n++;
+
+        }*/
+
+
+    }
+}
