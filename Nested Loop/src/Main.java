55586cf81304557f424a87f1c38c421515dedd4d
Muhammadsobir <muhammadsobirturdaliyev9199@gmail.com>
2025-07-05 11:17:40 -0700

    added for operator

--- Nested Loop/src/Main.java
+++ Nested Loop/src/Main.java
@@ -5,6 +5,15 @@ public class Main {
 
         // this is lesson Nested loop
 
+        /*for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(" " + i);
+            }
+            System.out.println();
+        }*/
+
+
+
 
 
     }
